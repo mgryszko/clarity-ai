87c516586e00105475fae3d3ee369b606a74df7d
mgryszko <marcin.gryszko@lastminute.com>
2021-02-15 19:47:55 +0100

    Parse connected hosts as long as there are no timestamps and implicit host to connect

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -2,9 +2,23 @@ import ch.tutteli.atrium.api.fluent.en_GB.toBe
 import ch.tutteli.atrium.api.verbs.expect
 import kotlin.test.Test
 
-class SmokeTest {
+class ParserTest {
     @Test
-    fun read() {
-        expect(true).toBe(false)
+    fun `connected hosts`() {
+        val log = """Shaquera
+        |Zidan
+        |Adalhi
+        """.trimMargin()
+
+        val hosts = parse(log)
+
+        expect(hosts).toBe(setOf(
+            "Shaquera",
+            "Zidan",
+            "Adalhi",
+        ))
     }
+
+    private fun parse(log: String): Set<String> =
+        log.lines().toSet()
 }
