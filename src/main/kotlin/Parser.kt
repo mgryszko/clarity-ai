dc3a00f7aadc92313cb432f4e993f76ff0c9cbd1
mgryszko <marcin.gryszko@lastminute.com>
2021-02-16 04:34:36 +0100

    Stream-process log file

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -1,18 +1,17 @@
 import java.io.File
 
 fun main(args : Array<String>) {
-    val log = File(args[0]).readText()
+    val logFileName = args[0]
     val connectedTo = args[1]
     val from = args[2].toLong()
     val to = args[3].toLong()
 
-    val hosts = parse(log = log, connectedTo = connectedTo, from = from, to = to)
+    val hosts = File(logFileName).useLines {
+        parse(lines = it, connectedTo = connectedTo, from = from, to = to)
+    }
 
     println(hosts)
 }
-
-fun parse(log: String, connectedTo: String, from: Long, to: Long): Set<String> =
-    parse(log.lines().asSequence(), connectedTo, from, to)
 
 fun parse(
     lines: Sequence<String>,
