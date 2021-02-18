4ed481df0a178a0e8d16a706053ceb65d2a794f2
mgryszko <marcin.gryszko@lastminute.com>
2021-02-18 22:08:57 +0100

    Use BufferedReader directly to prepare for tailing

--- src/main/kotlin/PeriodicReports.kt
+++ src/main/kotlin/PeriodicReports.kt
@@ -16,32 +16,30 @@ fun handlePeriodicReports(
     reportPeriodMs: Long,
     maxTolerableLagMs: Long,
 ) {
-    read(logFileName) { lines ->
-        val parsedLines = parse(lines)
-        val reportGenerator = PeriodicReportGenerator(
-            Host(host),
-            firstLine(logFileName).timestamp,
-            Duration(reportPeriodMs),
-            Duration(maxTolerableLagMs)
-        )
-        generatePeriodicReports(
-            lines = parsedLines,
-            reportCollector = collector,
-            reportGenerator = reportGenerator
-        )
+    val reportGenerator = PeriodicReportGenerator(
+        Host(host),
+        firstLine(logFileName).timestamp,
+        Duration(reportPeriodMs),
+        Duration(maxTolerableLagMs)
+    )
+    File(logFileName).bufferedReader().use { reader ->
+        var done = false
+        while (!done) {
+            val line = reader.readLine()
+            if (line != null) {
+                reportGenerator.processLogLine(parse(line), collector)
+            } else {
+                done = true
+            }
+        }
+        collector.closeReport()
     }
 }
-
-private fun <T> read(fileName: String, processLines: (Sequence<String>) -> T): T =
-    File(fileName).useLines(Charsets.UTF_8, processLines)
 
 private fun firstLine(logFileName: String): LogLine {
     val line = File(logFileName).useLines { it.first() }
     return parse(line)
 }
-
-private fun parse(lines: Sequence<String>): Sequence<LogLine> =
-    lines.filter(String::isNotBlank).map(::parse)
 
 fun generatePeriodicReports(
     lines: Sequence<LogLine>,
