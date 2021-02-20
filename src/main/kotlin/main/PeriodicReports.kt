067bc5ba74be7def288505ac853834096deba5d5
mgryszko <marcin.gryszko@lastminute.com>
2021-02-20 07:15:39 +0100

    Output incoming connections from host and top outgoing connections

--- src/main/kotlin/main/PeriodicReports.kt
+++ src/main/kotlin/main/PeriodicReports.kt
@@ -11,7 +11,11 @@ import log.Host
 import periodicreports.PeriodicReportsHandler
 import periodicreports.ReportCollector
 import periodicreports.incomingConnectionToTarget
+import periodicreports.onIncomingConnection
 import periodicreports.onOutgoingConnection
+import periodicreports.outgoingConnectionFromSource
+import periodicreports.pass
+import periodicreports.topOutgoingConnections
 import java.io.File
 
 class PeriodicReports : CliktCommand() {
@@ -20,18 +24,21 @@ class PeriodicReports : CliktCommand() {
     private val thirtySeconds = (30 * 1000).toLong()
 
     private val logFileName: String by argument(name = "LOG_FILE_NAME", help = "log file name")
-    private val host: String by argument(help = "host for reports")
+    private val host: String by argument(help = "host to report both incoming and outgoing connections")
     private val reportFreqMs: Long by option("-f", "--frequency", help = "reporting frequency").long().default(oneHour)
     private val maxTolerableLagMs: Long by option("-l", "--max-lag", help = "maximum tolerable lag of log entries")
         .long().default(fiveMinutes)
     private val timeoutMs: Long by option("-t", "--timeout", help = "log inactivity timeout").long().default(thirtySeconds)
 
     override fun run() {
+        val collector = ReportCollector(::println)
         PeriodicReportsHandler(
             logReader = FileLogReader(File(logFileName), Duration(timeoutMs)),
-            emitter = ReportCollector(::println),
+            emitter = collector,
             actionsByFilters = mapOf(
-                incomingConnectionToTarget(Host(host)) to onOutgoingConnection(ReportCollector(::println))
+                incomingConnectionToTarget(Host(host)) to onOutgoingConnection(collector),
+                outgoingConnectionFromSource(Host(host)) to onIncomingConnection(collector),
+                pass to topOutgoingConnections(collector),
             )
         ).handle(
             Duration(reportFreqMs),
