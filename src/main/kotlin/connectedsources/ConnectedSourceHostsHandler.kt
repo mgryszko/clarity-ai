a36bcaa4cfb5677acb2c4ee9ff17819720c34a05
mgryszko <marcin.gryszko@lastminute.com>
2021-02-20 11:56:48 +0100

    Inline function

--- src/main/kotlin/connectedsources/ConnectedSourceHostsHandler.kt
+++ src/main/kotlin/connectedsources/ConnectedSourceHostsHandler.kt
@@ -2,35 +2,20 @@ package connectedsources
 
 import kotlinx.coroutines.runBlocking
 import log.Host
-import log.LogLine
 import log.LogReader
 import log.Timestamp
 
 class ConnectedSourceHostsHandler(private val logReader: LogReader) {
     fun handle(target: String, fromMs: Long, toMs: Long, onSourceHosts: (Set<Host>) -> Unit) {
-        val logLines = mutableListOf<LogLine>()
+        val hosts = mutableSetOf<Host>()
         runBlocking {
-            logReader.readLines { line -> logLines.add(line) }
+            logReader.readLines { (timestamp, source, lineTarget) ->
+                if (lineTarget == Host(target) && timestamp >= Timestamp(fromMs) && timestamp <= Timestamp(toMs)) {
+                    hosts.add(source)
+                }
+            }
         }
-        val hosts = findSourceHosts(
-            lines = logLines.asSequence(),
-            target = Host(target),
-            from = Timestamp(fromMs),
-            to = Timestamp(toMs)
-        )
 
         onSourceHosts(hosts)
     }
-
-    private fun findSourceHosts(
-        lines: Sequence<LogLine>,
-        target: Host,
-        from: Timestamp,
-        to: Timestamp
-    ): Set<Host> = lines
-        .filter { (timestamp, _, lineTarget) ->
-            lineTarget == target && timestamp >= from && timestamp <= to
-        }
-        .map(LogLine::source)
-        .toSet()
 }
