cb5cae5c68536269eb38ab382c2cff4f400f54bb
mgryszko <marcin.gryszko@lastminute.com>
2021-02-20 11:19:59 +0100

    User-friendly CLI for connected source hosts

--- src/main/kotlin/main/ConnectedSourceHosts.kt
+++ src/main/kotlin/main/ConnectedSourceHosts.kt
@@ -1,18 +1,39 @@
 package main
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.default
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.long
+import file.FileLogReader
+import log.Duration
 import log.Host
 import log.LogLine
 import log.Timestamp
+import periodicreports.PeriodicReportsHandler
+import periodicreports.ReportCollector
+import periodicreports.incomingConnectionToTarget
+import periodicreports.onIncomingConnection
+import periodicreports.onOutgoingConnection
+import periodicreports.outgoingConnectionFromSource
+import periodicreports.pass
+import periodicreports.topOutgoingConnections
+import renderer.PrintStreamReportRenderer
 import java.io.File
 
-fun main(args: Array<String>) {
-    val logFileName = args[0]
-    val target = args[1]
-    val fromMs = args[2].toLong()
-    val toMs = args[3].toLong()
+class ConnectedSourceHosts : CliktCommand() {
+    private val logFileName: String by argument(name = "LOG_FILE_NAME", help = "log file name")
+    private val target: String by argument(help = "search target of incoming connections")
+    private val fromMs: Long by option("-f", "--from", help = "timestamp from inclusive").long().default(0)
+    private val toMs: Long by option("-t", "--to", help = "timestamp to inclusive").long().default(Long.MAX_VALUE)
 
-    handleConnectedSourceHosts(logFileName, target, fromMs, toMs, ::println)
+    override fun run() {
+        handleConnectedSourceHosts(logFileName, target, fromMs, toMs, ::println)
+    }
 }
+
+fun main(args: Array<String>) = ConnectedSourceHosts().main(args)
 
 fun handleConnectedSourceHosts(logFileName: String, target: String, fromMs: Long, toMs: Long, onSourceHosts: (Set<Host>) -> Unit) {
     val hosts = read(logFileName) { lines ->
